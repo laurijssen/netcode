439547ab4259278eb51f4291c642d4d5a94f2619
serve laurijssen <lauserve@homail.com>
2020-04-23 03:45:42 -0400

    sniff one action

--- sniffer.py
+++ sniffer.py
@@ -12,8 +12,8 @@ class IP(Structure):
             ("ttl", c_ubyte),
             ("protocol_num", c_ubyte),
             ("sum", c_ushort),
-            ("src", c_ulong),
-            ("dst", c_ulong)
+            ("src", c_uint32),
+            ("dst", c_uint32)
     ]
 
     def __new__(self, socket_buffer=None):
@@ -21,8 +21,8 @@ class IP(Structure):
 
     def __init__(self, socket_buffer=None):
         self.protocol_map = {1:"ICMP", 6:"TCP", 17:"UDP"}
-        self.src_address = socket.inet_ntoa(struct.pack("<L", selfc.src))
-        self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+        self.src_address = socket.inet_ntoa(struct.pack("@I", self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("@I", self.dst))
 
         try:
             self.protocol = self.protocol_map[self.protocol_num]
@@ -40,11 +40,11 @@ sniffer.bind((host, 0))
 sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
 try:
-    raw_buff = sniffer.recvfrom(65535)
+    raw_buff = sniffer.recvfrom(65535)[0]
 
     ip_header = IP(raw_buff[0:20])
 
-    print("protocol {}".format(ip_header.protocol))
+    print("protocol {} src {} dst {}".format(ip_header.protocol, ip_header.src_address, ip_header.dst_address))
 except KeyboardInterrupt:
     pass
 
